e168e802a69130608c4a1e8bea942cb982eadd1e
Giada15 <tascheriogiada@gmail.com>
2024-04-23 09:58:31 -0400

    Refactor: Added a for loop to print a message

--- Exam.java
+++ Exam.java
@@ -4,11 +4,17 @@ public class Exam {
         String[] seats = { "Harry", "Neville", "Ron", "Hermione", "Seamus"};
 
         System.out.println("It's time to take your 5th year exams. Please, take your seats.\n");
-        System.out.println(seats[0] + ", you will take seat 0");
-        System.out.println(seats[1] + ", you will take seat 1");
-        System.out.println(seats[2] + ", you will take seat 2");
-        System.out.println(seats[3] + ", you will take seat 3");
-        System.out.println(seats[4] + ", you will take seat 4");
+        // System.out.println(seats[0] + ", you will take seat 0");
+        // System.out.println(seats[1] + ", you will take seat 1");
+        // System.out.println(seats[2] + ", you will take seat 2");
+        // System.out.println(seats[3] + ", you will take seat 3");
+        // System.out.println(seats[4] + ", you will take seat 4");
+
+        for(int i=0; i<seats.length; i++){
+            System.out.println(seats[i] + ", you will take seat " + i);
+        }
+
+
     }
     
 }
