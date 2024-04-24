5cf78ca1b6f3d22060159f8bfe37337f31c5c887
Giada15 <tascheriogiada@gmail.com>
2024-04-24 10:17:11 -0400

    Practiced iterating over an array with a for loop to print messages based on user input

--- Grocer.java
+++ Grocer.java
@@ -1,13 +1,20 @@
+import java.util.Scanner;
+
 public class Grocer {
 
     public static void main(String[] args) {
+
         
+        Scanner scan = new Scanner(System.in);
         String[] aisles = {"apples", "bananas", "candy", "chocolate", "coffee", "tea"};
     
-        System.out.println("\nDo you sell coffee?");
+
+        System.out.println("Welcome to Java Grocers.");
+        System.out.println("What can I help you find?");
+        String aisle = scan.nextLine();
 
         for(int i=0; i<aisles.length; i++){
-            if(aisles[i] == "coffee"){
+            if(aisles[i].equals(aisle)){
                 System.out.println("\nWe have that in aisle: " + i);
             }
         }
