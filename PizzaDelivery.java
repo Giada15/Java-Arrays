5813731787befe7be390ca12ee5dd5eb2f11e626
Giada15 <tascheriogiada@gmail.com>
2024-04-24 12:57:13 -0400

    Practiced updating arrays

---
+++ PizzaDelivery.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PizzaDelivery {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("How many pizza toppings do you want?");
+        int toppingsTot = scan.nextInt();
+        scan.nextLine();
+
+        String[] toppings = new String[toppingsTot];
+ 
+        System.out.println("Great, enter each topping!");
+        for(int i=0; i<toppings.length; i++){
+            System.out.print(i + ". ");
+            toppings[i] = scan.nextLine();
+        }
+        
+        System.out.println("Thank you! Here are the toppings you ordered:");
+        for(int i=0; i<toppings.length; i++){
+            System.out.println(i + ". " + toppings[i]);
+        }
+    }
+    
+}
