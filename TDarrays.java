05472eae26cca7d0103368098bbc25037cef39e0
Giada15 <tascheriogiada@gmail.com>
2024-04-25 10:13:51 -0400

    Refactor: Added nested for loop to print grades from 2D array

--- TDarrays.java
+++ TDarrays.java
@@ -19,18 +19,38 @@ public class TDarrays {
         // grades[2][2] = 99;
         // grades[2][3] = 100;
 
+        // System.out.println("\tHarry: " + grades[0][0] + " " + grades[0][1] + " " + grades[0][2] + " " + grades[0][3]);
+
+        // System.out.println("\tRon: " +  grades[1][0] + " " + grades[1][1] + " " + grades[1][2] + " " + grades[1][3]);
+
+        // System.out.println("\tHermione: " + grades[2][0] + " " + grades[2][1] + " " + grades[2][2] + " " + grades[2][3]);
+
         int [][] grades = {
             {72, 74, 78, 76},
             {65, 64, 61, 67},
             {95, 98, 99, 100}
         };
 
-        System.out.println("\tHarry: " + grades[0][0] + " " + grades[0][1] + " " + grades[0][2] + " " + grades[0][3]);
+        // for (int j=0; j<grades[0].length; j++){
+        //     System.out.print(grades[0][j] + " " );
+        // }
+        // System.out.println("");
 
-        System.out.println("\tRon: " +  grades[1][0] + " " + grades[1][1] + " " + grades[1][2] + " " + grades[1][3]);
 
-        System.out.println("\tHermione: " + grades[2][0] + " " + grades[2][1] + " " + grades[2][2] + " " + grades[2][3]);
+        //nested loop
+        for(int i=0; i<grades.length; i++){
+            switch(i){
+                case 0: System.out.print("\tHarry: "); break;
+                case 1: System.out.print("\tRon: "); break;
+                case 2: System.out.print("\tHermione: "); break;
+                default: System.out.println("Error");
+            }
 
+            for(int j=0; j<grades[i].length; j++){
+                System.out.print(grades[i][j] + " ");
+            }
+            System.out.println("");
+        }
 
     }
 }
