2b308cf2380a97c3c4933e59d8f49a9075c1b294
Giada15 <tascheriogiada@gmail.com>
2024-04-25 10:29:45 -0400

    Refactor: Added nested for loop to print prices from 2D array

--- Prices.java
+++ Prices.java
@@ -21,17 +21,33 @@ public class Prices {
         // prices[2][3] = 9.99;
         // prices[2][4] = 10.99;
 
+        // System.out.println("Baking: " + prices[0][0] + " " + prices[0][1] + " " + prices[0][2] + " " + prices[0][3] + " " + prices[0][4]);
+
+        // System.out.println("Beverage " + " " + prices[1][0] + " " + prices[1][1] + " " + prices[1][2] + " " + prices[1][3] + " " + prices[1][4]);
+
+        // System.out.println("Cereals: " + " " + prices[2][0] + " " + prices[2][1] + " " + prices[2][2] + " " + prices[2][3] + " " + prices[2][4]);
+
         double[][] prices = {
             {12.99, 8.99, 9.99, 10.49, 11.99},
             {0.99, 1.99, 2.49, 1.49, 2.99},
             {8.99, 7.99, 9.49, 9.99, 10.99}
         };
 
-        System.out.println("Baking: " + prices[0][0] + " " + prices[0][1] + " " + prices[0][2] + " " + prices[0][3] + " " + prices[0][4]);
+        for(int i=0; i<prices.length; i++){
+            switch(i){
+                case 0: System.out.print("Baking: ");break;
+                case 1: System.out.print("Beverage: ");break;
+                case 2: System.out.print("Cereals: ");break;
+                default: System.out.println("Error");
+            }
+            for(int j=0; j<prices[i].length; j++){
+                System.out.print(prices[i][j] + " ");
+            }
+            System.out.println("");
+        }
 
-        System.out.println("Beverage " + " " + prices[1][0] + " " + prices[1][1] + " " + prices[1][2] + " " + prices[1][3] + " " + prices[1][4]);
 
-        System.out.println("Cereals: " + " " + prices[2][0] + " " + prices[2][1] + " " + prices[2][2] + " " + prices[2][3] + " " + prices[2][4]);
+        
     }
     
 }
